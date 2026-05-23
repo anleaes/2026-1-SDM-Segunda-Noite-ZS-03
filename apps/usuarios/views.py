dc55004aeecd1d6d6e8137c0b3011e8761668adb
augusto-dmh <augustodemelohenriques@gmail.com>
2026-05-23 19:15:00 -0300

    feat(usuarios): add UsuarioViewSet with ModelViewSet

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .models import Usuario
+from rest_framework import viewsets
+from .serializer import UsuarioSerializer
 
-# Create your views here.
+
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
