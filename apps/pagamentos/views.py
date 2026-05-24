ad82f19e39bf0dd44461e554c2df16bc29078aa4
Vitor Appel Teixeira <135709356+VitorAppelTeixeira@users.noreply.github.com>
2026-05-24 14:59:26 -0300

    feat(pagamentos): add PagamentoViewSet (#31)
    * feat(pagamentos): add PagamentoViewSet with ModelViewSet
    
    * feat(pagamentos): ViewSet ready for URL routing
    
    ---------
    
    Co-authored-by: augusto-dmh <augustodemelohenriques@gmail.com>

--- apps/pagamentos/views.py
+++ apps/pagamentos/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .models import Pagamento
+from rest_framework import viewsets
+from .serializer import PagamentoSerializer
 
-# Create your views here.
+
+class PagamentoViewSet(viewsets.ModelViewSet):
+    queryset = Pagamento.objects.all()
+    serializer_class = PagamentoSerializer
