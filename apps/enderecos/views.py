877c7c7fc57403208042c99a2456a605d85e94b9
augusto-dmh <augustodemelohenriques@gmail.com>
2026-05-23 22:45:00 -0300

    feat(enderecos): add EnderecoViewSet with ModelViewSet

--- apps/enderecos/views.py
+++ apps/enderecos/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .models import Endereco
+from rest_framework import viewsets
+from .serializer import EnderecoSerializer
 
-# Create your views here.
+
+class EnderecoViewSet(viewsets.ModelViewSet):
+    queryset = Endereco.objects.all()
+    serializer_class = EnderecoSerializer
