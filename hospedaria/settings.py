29c0b5843e26b83db130240d3241da625dc5578f
Vitor Appel Teixeira <135709356+VitorAppelTeixeira@users.noreply.github.com>
2026-05-24 14:56:57 -0300

    feat(comodidades): add Comodidade model (#9)
    * feat(comodidades): create app and add Comodidade model
    
    * feat(comodidades): register comodidades app in INSTALLED_APPS
    
    ---------
    
    Co-authored-by: augusto-dmh <augustodemelohenriques@gmail.com>

--- hospedaria/settings.py
+++ hospedaria/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [    'comodidades.apps.ComodidadesConfig',
+]
 
 
 # Application definition
