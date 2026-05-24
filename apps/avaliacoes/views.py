1816a2e8647fa1c7d06c987ce0b09d77e613480b
Vitor Appel Teixeira <135709356+VitorAppelTeixeira@users.noreply.github.com>
2026-05-24 14:59:51 -0300

    feat(avaliacoes): add AvaliacaoViewSet (#35)
    * feat(avaliacoes): add AvaliacaoViewSet with ModelViewSet
    
    * feat(avaliacoes): ViewSet ready for URL routing
    
    ---------
    
    Co-authored-by: augusto-dmh <augustodemelohenriques@gmail.com>

--- apps/avaliacoes/views.py
+++ apps/avaliacoes/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .models import Avaliacao
+from rest_framework import viewsets
+from .serializer import AvaliacaoSerializer
 
-# Create your views here.
+
+class AvaliacaoViewSet(viewsets.ModelViewSet):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
