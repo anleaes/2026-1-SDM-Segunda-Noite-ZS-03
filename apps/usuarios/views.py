5f366743c53c93902a092b70ebd8e1905c5186fa
augusto-dmh <augustodemelohenriques@gmail.com>
2026-05-24 20:52:00 -0300

    ViewSet basico do usuarios

---
+++ apps/usuarios/views.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from .models import Usuarios
+from .serializer import UsuariosSerializer
+
+class UsuariosViewSet(viewsets.ModelViewSet):
+    queryset = Usuarios.objects.all()
+    serializer_class = UsuariosSerializer
