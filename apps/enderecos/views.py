6183712e692989045d0194066e39d821498363a0
augusto-dmh <augustodemelohenriques@gmail.com>
2026-05-24 21:57:00 -0300

    adiciona autenticacao token no enderecos

--- apps/enderecos/views.py
+++ apps/enderecos/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
-from .models import Enderecos
-from .serializer import EnderecosSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from .models import Endereco
+from .serializer import EnderecoSerializer
 
-class EnderecosViewSet(viewsets.ModelViewSet):
-    queryset = Enderecos.objects.all()
-    serializer_class = EnderecosSerializer
+class EnderecoViewSet(viewsets.ModelViewSet):
+    queryset = Endereco.objects.all()
+    serializer_class = EnderecoSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
