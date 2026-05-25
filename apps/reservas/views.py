cb6b95c346355d6790f62a895eada298200306df
augusto-dmh <augustodemelohenriques@gmail.com>
2026-05-25 03:10:00 -0300

    feat(reservas): add ReservaViewSet with ModelViewSet

--- apps/reservas/views.py
+++ apps/reservas/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .models import Reserva
+from rest_framework import viewsets
+from .serializer import ReservaSerializer
 
-# Create your views here.
+
+class ReservaViewSet(viewsets.ModelViewSet):
+    queryset = Reserva.objects.all()
+    serializer_class = ReservaSerializer
