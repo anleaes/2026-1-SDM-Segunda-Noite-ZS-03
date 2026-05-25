41b4bbad12a37e69c6ccd3c87d61791315f768e8
Vitor Appel Teixeira <135709356+VitorAppelTeixeira@users.noreply.github.com>
2026-05-24 23:38:47 -0300

    serializer do hospedagens (#61)
    Co-authored-by: augusto-dmh <augustodemelohenriques@gmail.com>

---
+++ apps/hospedagens/serializer.py
@@ -0,0 +1,8 @@
+from .models import Hospedagem
+from rest_framework import serializers
+
+
+class HospedagemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hospedagem
+        fields = '__all__'
