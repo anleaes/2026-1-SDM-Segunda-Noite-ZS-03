d9d93a127eb20de29ef5a8855da276c0c73a17a6
augusto-dmh <augustodemelohenriques@gmail.com>
2026-05-25 22:29:00 -0300

    adiciona autenticacao token no pagamentos

--- apps/pagamentos/views.py
+++ apps/pagamentos/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
-from .models import Pagamentos
-from .serializer import PagamentosSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from .models import Pagamento
+from .serializer import PagamentoSerializer
 
-class PagamentosViewSet(viewsets.ModelViewSet):
-    queryset = Pagamentos.objects.all()
-    serializer_class = PagamentosSerializer
+class PagamentoViewSet(viewsets.ModelViewSet):
+    queryset = Pagamento.objects.all()
+    serializer_class = PagamentoSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
