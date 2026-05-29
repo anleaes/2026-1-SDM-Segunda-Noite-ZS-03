eeba4cc976d6c1b97d7d6308e48dc0f042cfcaaa
diegol447 <diegoamoreira33@gmail.com>
2026-05-29 20:53:49 -0300

    anfitrioes/models

--- apps/anfitrioes/models.py
+++ apps/anfitrioes/models.py
@@ -1,18 +1,38 @@
 from django.db import models
 
-class Anfitriao(models.Model):
-    nome = models.CharField("Nome", max_length=100)
-    email = models.EmailField("E-mail")
-    telefone = models.CharField("Telefone", max_length=20)
-    documento = models.CharField("Documento", max_length=20)
-    bio = models.TextField('Biografia', blank=True, null=True)
-    avaliacao_media = models.DecimalField('Avaliação Média', max_digits=3, decimal_places=2, default=0.0)
 
+class Anfitriao(models.Model):
+    nome = models.CharField(
+        verbose_name="Nome",
+        max_length=100,
+    )
+    email = models.EmailField(
+        verbose_name="E-mail",
+    )
+    telefone = models.CharField(
+        verbose_name="Telefone",
+        max_length=20,
+    )
+    documento = models.CharField(
+        verbose_name="Documento",
+        max_length=20,
+    )
+    bio = models.TextField(
+        verbose_name="Biografia",
+        blank=True,
+        null=True,
+    )
+    avaliacao_media = models.DecimalField(
+        verbose_name="Avaliação Média",
+        max_digits=3,
+        decimal_places=2,
+        default=0.0,
+    )
 
     class Meta:
-        verbose_name = 'Anfitrião'
-        verbose_name_plural = 'Anfitriões'
-        ordering = ['id']
+        verbose_name = "Anfitrião"
+        verbose_name_plural = "Anfitriões"
+        ordering = ["id"]
 
     def __str__(self):
         return f"Anfitrião: {self.nome} (ID: {self.id})"
