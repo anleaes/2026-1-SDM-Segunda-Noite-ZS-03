8087883eb41ec247add22c87c51760f7e28773df
Vitor Appel Teixeira <135709356+VitorAppelTeixeira@users.noreply.github.com>
2026-05-29 14:05:47 -0300

    Anfitrioes/models (#125)
    * feat(anfitrioes): melhora formatação da string de representacao do Anfitriao para melhor visualização
    
    * style(anfitrioes): dei uma padronizada leve nas strings

--- apps/anfitrioes/models.py
+++ apps/anfitrioes/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 class Anfitriao(models.Model):
-    nome = models.CharField('Nome', max_length=100)
-    email = models.EmailField('E-mail')
-    telefone = models.CharField('Telefone', max_length=20)
-    documento = models.CharField('Documento', max_length=20)
+    nome = models.CharField("Nome", max_length=100)
+    email = models.EmailField("E-mail")
+    telefone = models.CharField("Telefone", max_length=20)
+    documento = models.CharField("Documento", max_length=20)
     bio = models.TextField('Biografia', blank=True, null=True)
     avaliacao_media = models.DecimalField('Avaliação Média', max_digits=3, decimal_places=2, default=0.0)
+
 
     class Meta:
         verbose_name = 'Anfitrião'
@@ -14,4 +15,4 @@ class Anfitriao(models.Model):
         ordering = ['id']
 
     def __str__(self):
-        return f'{self.id} - {self.nome}'
+        return f"Anfitrião: {self.nome} (ID: {self.id})"
