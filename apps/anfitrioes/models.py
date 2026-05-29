ef1257fa259f1d8e62857b9ade2c7c15aafc6a40
Vitor Appel Teixeira <vitorappel2005@gmail.com>
2026-05-29 13:57:28 -0300

    style(anfitrioes): dei uma padronizada leve nas strings

--- apps/anfitrioes/models.py
+++ apps/anfitrioes/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 class Anfitriao(models.Model):
-    nome = models.CharField('Nome', max_length=100)
-    email = models.EmailField('E-mail')
-    telefone = models.CharField('Telefone', max_length=20)
-    documento = models.CharField('Documento', max_length=20)
+    nome = models.CharField("Nome", max_length=100)
+    email = models.EmailField("E-mail")
+    telefone = models.CharField("Telefone", max_length=20)
+    documento = models.CharField("Documento", max_length=20)
     bio = models.TextField('Biografia', blank=True, null=True)
     avaliacao_media = models.DecimalField('Avaliação Média', max_digits=3, decimal_places=2, default=0.0)
+
 
     class Meta:
         verbose_name = 'Anfitrião'
