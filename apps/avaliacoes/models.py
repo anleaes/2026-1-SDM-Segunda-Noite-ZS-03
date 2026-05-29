4bd4bdb285c58e3203b005c3bff45b62fb86831d
Vitor Appel Teixeira <vitorappel2005@gmail.com>
2026-05-29 20:56:51 -0300

    style(avaliacoes): padronização da string com aspas duplas

--- apps/avaliacoes/models.py
+++ apps/avaliacoes/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class Avaliacao(models.Model):
-    hospedagem = models.ForeignKey('hospedagens.Hospedagem', on_delete=models.CASCADE, verbose_name='Hospedagem')
-    nome_hospede = models.CharField('Nome do Hóspede', max_length=100)
-    email = models.EmailField('E-mail')
-    nota = models.PositiveIntegerField('Nota')
-    comentario = models.TextField('Comentário', blank=True, null=True)
-    data_avaliacao = models.DateTimeField('Data da Avaliação', auto_now_add=True)
+    hospedagem = models.ForeignKey("hospedagens.Hospedagem", on_delete=models.CASCADE, verbose_name='Hospedagem')
+    nome_hospede = models.CharField("Nome do Hóspede", max_length=100)
+    email = models.EmailField("E-mail")
+    nota = models.PositiveIntegerField("Nota")
+    comentario = models.TextField("Comentário", blank=True, null=True)
+    data_avaliacao = models.DateTimeField("Data da Avaliação", auto_now_add=True)
 
     class Meta:
         verbose_name = 'Avaliação'
