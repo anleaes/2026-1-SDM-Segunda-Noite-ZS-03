4a5892eb14547e2bd7bb7e440dd11d065e68801a
diegol447 <diegoamoreira33@gmail.com>
2026-05-30 17:06:43 -0300

    avalicoes/models

--- apps/avaliacoes/models.py
+++ apps/avaliacoes/models.py
@@ -1,17 +1,36 @@
 from django.db import models
 
+
 class Avaliacao(models.Model):
-    hospedagem = models.ForeignKey("hospedagens.Hospedagem", on_delete=models.CASCADE, verbose_name='Hospedagem')
-    nome_hospede = models.CharField("Nome do Hóspede", max_length=100)
-    email = models.EmailField("E-mail")
-    nota = models.PositiveIntegerField("Nota")
-    comentario = models.TextField("Comentário", blank=True, null=True)
-    data_avaliacao = models.DateTimeField("Data da Avaliação", auto_now_add=True)
+    hospedagem = models.ForeignKey(
+        "hospedagens.Hospedagem",
+        verbose_name="Hospedagem",
+        on_delete=models.CASCADE,
+    )
+    nome_hospede = models.CharField(
+        verbose_name="Nome do Hóspede",
+        max_length=100,
+    )
+    email = models.EmailField(
+        verbose_name="E-mail",
+    )
+    nota = models.PositiveIntegerField(
+        verbose_name="Nota",
+    )
+    comentario = models.TextField(
+        verbose_name="Comentário",
+        blank=True,
+        null=True,
+    )
+    data_avaliacao = models.DateTimeField(
+        verbose_name="Data da Avaliação",
+        auto_now_add=True,
+    )
 
     class Meta:
-        verbose_name = 'Avaliação'
-        verbose_name_plural = 'Avaliações'
-        ordering = ['id']
+        verbose_name = "Avaliação"
+        verbose_name_plural = "Avaliações"
+        ordering = ["id"]
 
     def __str__(self):
         return f"Avaliação #{self.id}: Nota {self.nota} para {self.hospedagem}"
