9daa8cb2cdfd183ff1638547db886b425fbda4a4
Vitor Appel Teixeira <135709356+VitorAppelTeixeira@users.noreply.github.com>
2026-05-29 21:06:28 -0300

    Avaliacoes/models (#129)
    * style(avaliacoes): padronização da string com aspas duplas
    
    * refactor(avaliacoes): melhorei o formato de exibição da avaliação

--- apps/avaliacoes/models.py
+++ apps/avaliacoes/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class Avaliacao(models.Model):
-    hospedagem = models.ForeignKey('hospedagens.Hospedagem', on_delete=models.CASCADE, verbose_name='Hospedagem')
-    nome_hospede = models.CharField('Nome do Hóspede', max_length=100)
-    email = models.EmailField('E-mail')
-    nota = models.PositiveIntegerField('Nota')
-    comentario = models.TextField('Comentário', blank=True, null=True)
-    data_avaliacao = models.DateTimeField('Data da Avaliação', auto_now_add=True)
+    hospedagem = models.ForeignKey("hospedagens.Hospedagem", on_delete=models.CASCADE, verbose_name='Hospedagem')
+    nome_hospede = models.CharField("Nome do Hóspede", max_length=100)
+    email = models.EmailField("E-mail")
+    nota = models.PositiveIntegerField("Nota")
+    comentario = models.TextField("Comentário", blank=True, null=True)
+    data_avaliacao = models.DateTimeField("Data da Avaliação", auto_now_add=True)
 
     class Meta:
         verbose_name = 'Avaliação'
@@ -14,4 +14,4 @@ class Avaliacao(models.Model):
         ordering = ['id']
 
     def __str__(self):
-        return f'{self.id} - {self.nota} - {self.hospedagem}'
+        return f"Avaliação #{self.id}: Nota {self.nota} para {self.hospedagem}"
