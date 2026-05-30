296fd0128725be98c143b295e29ac1084663c93b
Vitor Appel Teixeira <vitorappel2005@gmail.com>
2026-05-29 22:05:15 -0300

    style(comodidades): padronizei as aspas duplas

--- apps/comodidades/models.py
+++ apps/comodidades/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Comodidade(models.Model):
-    nome = models.CharField('Nome', max_length=100)
-    descricao = models.TextField('Descrição')
-    icone = models.CharField('Ícone', max_length=50, blank=True, null=True)
-    ativo = models.BooleanField('Ativo', default=True)
+    nome = models.CharField("Nome", max_length=100)
+    descricao = models.TextField("Descrição")
+    icone = models.CharField("Ícone", max_length=50, blank=True, null=True)
+    ativo = models.BooleanField("Ativo", default=True)
 
     class Meta:
         verbose_name = 'Comodidade'
