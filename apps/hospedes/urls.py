5197500a6dcde9856eef397cf4d8668499a5e723
diegol447 <diegoamoreira33@gmail.com>
2026-05-31 14:59:00 -0300

    padronizacoes no codigo

--- apps/hospedes/urls.py
+++ apps/hospedes/urls.py
@@ -1,14 +1,13 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework import routers
 
 from . import views
 
-app_name = 'hospedes'
+app_name = "hospedes"
 
 router = routers.DefaultRouter()
-router.register('', views.HospedeViewSet, basename='hospedes')
+router.register("", views.HospedeViewSet, basename="hospedes")
 
 urlpatterns = [
-    path('', include(router.urls))
+    path("", include(router.urls))
 ]
-
