fb52a628a40c78b1f266456760ca771e3b7b0bda
diegol447 <diegoamoreira33@gmail.com>
2026-05-31 15:35:22 -0300

    melhorias de legibilidade do codigo

--- apps/mensagens/models.py
+++ apps/mensagens/models.py
@@ -2,14 +2,39 @@ from django.db import models
 
 
 class Mensagem(models.Model):
-    hospedagem = models.ForeignKey("hospedagens.Hospedagem", on_delete=models.CASCADE, verbose_name="Hospedagem")
-    nome = models.CharField("Nome", max_length=100)
-    email = models.EmailField("E-mail")
-    telefone = models.CharField("Telefone", max_length=20, blank=True, null=True)
-    assunto = models.CharField("Assunto", max_length=200)
-    mensagem = models.TextField("Mensagem")
-    lida = models.BooleanField("Lida", default=False)
-    enviada_em = models.DateTimeField("Enviada em", auto_now_add=True)
+    hospedagem = models.ForeignKey(
+        "hospedagens.Hospedagem",
+        verbose_name="Hospedagem",
+        on_delete=models.CASCADE,
+    )
+    nome = models.CharField(
+        verbose_name="Nome",
+        max_length=100,
+    )
+    email = models.EmailField(
+        verbose_name="E-mail",
+    )
+    telefone = models.CharField(
+        verbose_name="Telefone",
+        max_length=20,
+        blank=True,
+        null=True,
+    )
+    assunto = models.CharField(
+        verbose_name="Assunto",
+        max_length=200,
+    )
+    mensagem = models.TextField(
+        verbose_name="Mensagem",
+    )
+    lida = models.BooleanField(
+        verbose_name="Lida",
+        default=False,
+    )
+    enviada_em = models.DateTimeField(
+        verbose_name="Enviada em",
+        auto_now_add=True,
+    )
 
     class Meta:
         verbose_name = "Mensagem"
