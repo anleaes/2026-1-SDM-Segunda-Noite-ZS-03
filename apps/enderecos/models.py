5dd4861e3815bc00861be0cd92d395b555adc437
diegol447 <diegoamoreira33@gmail.com>
2026-05-31 10:34:50 -0300

    enderecos/models

--- apps/enderecos/models.py
+++ apps/enderecos/models.py
@@ -1,13 +1,37 @@
 from django.db import models
 
+
 class Endereco(models.Model):
-    logradouro = models.CharField("Logradouro", max_length=200)
-    numero = models.CharField("Número", max_length=10)
-    complemento = models.CharField("Complemento", max_length=100, blank=True, null=True)
-    bairro = models.CharField("Bairro", max_length=100)
-    cidade = models.CharField("Cidade", max_length=100)
-    estado = models.CharField("Estado", max_length=2)
-    cep = models.CharField("CEP", max_length=9)
+    logradouro = models.CharField(
+        verbose_name="Logradouro",
+        max_length=200,
+    )
+    numero = models.CharField(
+        verbose_name="Número",
+        max_length=10,
+    )
+    complemento = models.CharField(
+        verbose_name="Complemento",
+        max_length=100,
+        blank=True,
+        null=True,
+    )
+    bairro = models.CharField(
+        verbose_name="Bairro",
+        max_length=100,
+    )
+    cidade = models.CharField(
+        verbose_name="Cidade",
+        max_length=100,
+    )
+    estado = models.CharField(
+        verbose_name="Estado",
+        max_length=2,
+    )
+    cep = models.CharField(
+        verbose_name="CEP",
+        max_length=9,
+    )
 
     class Meta:
         verbose_name = "Endereço"
@@ -15,4 +39,7 @@ class Endereco(models.Model):
         ordering = ["id"]
 
     def __str__(self):
-        return f"Endereço{self.id}: {self.logradouro}, {self.numero} - {self.cidade}/{self.estado}"
+        return (
+            f"Endereço{self.id}: "
+            f"{self.logradouro}, {self.numero} - {self.cidade}/{self.estado}"
+        )
