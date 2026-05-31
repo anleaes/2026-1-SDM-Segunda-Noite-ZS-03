3394c0186674b44b5aac2e6cdf6c8b0353bff147
diegol447 <diegoamoreira33@gmail.com>
2026-05-31 10:16:23 -0300

    comodidades/models

--- apps/comodidades/models.py
+++ apps/comodidades/models.py
@@ -1,15 +1,29 @@
 from django.db import models
 
+
 class Comodidade(models.Model):
-    nome = models.CharField("Nome", max_length=100)
-    descricao = models.TextField("Descrição")
-    icone = models.CharField("Ícone", max_length=50, blank=True, null=True)
-    ativo = models.BooleanField("Ativo", default=True)
+    nome = models.CharField(
+        verbose_name="Nome",
+        max_length=100,
+    )
+    descricao = models.TextField(
+        verbose_name="Descrição",
+    )
+    icone = models.CharField(
+        verbose_name="Ícone",
+        max_length=50,
+        blank=True,
+        null=True,
+    )
+    ativo = models.BooleanField(
+        verbose_name="Ativo",
+        default=True,
+    )
 
     class Meta:
-        verbose_name = 'Comodidade'
-        verbose_name_plural = 'Comodidades'
-        ordering = ['id']
+        verbose_name = "Comodidade"
+        verbose_name_plural = "Comodidades"
+        ordering = ["id"]
 
     def __str__(self):
         return f"Comodidade #{self.id}: {self.nome}"
