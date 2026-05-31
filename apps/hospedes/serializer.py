1f68d94d51ab337fd35804a8ad238149c34aac54
Vitor Appel Teixeira <vitorappel2005@gmail.com>
2026-05-31 10:39:58 -0300

    style(hospedes): padronizei as aspas para melhor visualização

--- apps/hospedes/serializer.py
+++ apps/hospedes/serializer.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class HospedeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hospede
-        fields = '__all__'
+        fields = "__all__"
