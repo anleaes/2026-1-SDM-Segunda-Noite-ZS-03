12ad35f54aabe131d32615dc40e40ebb8dc39a06
diegol447 <diegoamoreira33@gmail.com>
2026-05-31 14:44:49 -0300

    melhorias de legibilidade

--- apps/hospedes/models.py
+++ apps/hospedes/models.py
@@ -2,12 +2,28 @@ from django.db import models
 
 
 class Hospede(models.Model):
-    nome = models.CharField("Nome", max_length=100)
-    email = models.EmailField("E-mail")
-    telefone = models.CharField("Telefone", max_length=20)
-    documento = models.CharField("Documento", max_length=20)
-    nacionalidade = models.CharField("Nacionalidade", max_length=50)
-    data_nascimento = models.DateField("Data de Nascimento")
+    nome = models.CharField(
+        verbose_name="Nome",
+        max_length=100,
+    )
+    email = models.EmailField(
+        verbose_name="E-mail",
+    )
+    telefone = models.CharField(
+        verbose_name="Telefone",
+        max_length=20,
+    )
+    documento = models.CharField(
+        verbose_name="Documento",
+        max_length=20,
+    )
+    nacionalidade = models.CharField(
+        verbose_name="Nacionalidade",
+        max_length=50,
+    )
+    data_nascimento = models.DateField(
+        verbose_name="Data de Nascimento",
+    )
 
     class Meta:
         verbose_name = "Hóspede"
