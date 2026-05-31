7c0ed0129399c295c0416f8ca8db3e1b49318c6e
Vitor Appel Teixeira <vitorappel2005@gmail.com>
2026-05-31 14:15:49 -0300

    feat(usuarios): deixei os campos mais explicitos

--- apps/usuarios/serializer.py
+++ apps/usuarios/serializer.py
@@ -4,4 +4,5 @@ from rest_framework import serializers
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
-        fields = '__all__'
+        fields = ["id", "nome", "email", "telefone", "cpf", "data_nascimento", "ativo", "criado_em",]
+        
