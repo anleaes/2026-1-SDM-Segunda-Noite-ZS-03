a2ab9c01d73759172da93eb55813575f93628e64
diegol447 <diegoamoreira33@gmail.com>
2026-05-31 10:41:03 -0300

    reorganizei os atributos serializer/enderecos

--- apps/enderecos/serializer.py
+++ apps/enderecos/serializer.py
@@ -2,7 +2,17 @@ from rest_framework import serializers
 
 from .models import Endereco
 
+
 class EnderecoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Endereco
-        fields = ("id", "logradouro", "numero", "complemento", "bairro", "cidade", "estado", "cep")
+        fields = (
+            "id",
+            "logradouro",
+            "numero",
+            "complemento",
+            "bairro",
+            "cidade",
+            "estado",
+            "cep",
+        )
