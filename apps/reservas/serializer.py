7d172934d63820246b22e9d67e96e95366d7ae69
Vitor Appel Teixeira <vitorappel2005@gmail.com>
2026-05-31 13:48:23 -0300

    refactor(reservas): integrei o def validate para validar a data do check-out

--- apps/reservas/serializer.py
+++ apps/reservas/serializer.py
@@ -5,4 +5,8 @@ class ReservaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reserva
         fields = ["id", "hospedagem", "hospede", "data_checkin", "data_checkout", "quantidade_hospedes", "valor_total", "status", "criada_em",]
-        
+
+    def validate(self, data):
+        if data["data_checkout"] <= data["data_checkin"]:
+            raise serializers.ValidationError("A data de check-out deve ser posterior à data de check-in.")
+        return data
