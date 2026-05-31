e28c58a5a9fc6ccc7ef9391833546c7da01c4826
diegol447 <diegoamoreira33@gmail.com>
2026-05-31 10:26:19 -0300

    comodidades/urls

--- apps/comodidades/urls.py
+++ apps/comodidades/urls.py
@@ -1,13 +1,13 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework import routers
 
 from . import views
 
-app_name = 'comodidades'
+app_name = "comodidades"
 
 router = routers.DefaultRouter()
-router.register('', views.ComodidadeViewSet, basename='comodidades')
+router.register("", views.ComodidadeViewSet, basename="comodidades")
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
 ]
