bbd5bb649214f1626e97a9f173d526d39afb59af
diegol447 <diegoamoreira33@gmail.com>
2026-05-31 15:40:11 -0300

    alterações no codigo

--- apps/mensagens/serializer.py
+++ apps/mensagens/serializer.py
@@ -2,7 +2,8 @@ from rest_framework import serializers
 
 from .models import Mensagem
 
+
 class MensagemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mensagem
-        fields = ["id", "hospedagem", "nome", "email", "telefone", "assunto", "mensagem", "lida", "enviada_em",]
+        fields = ["id", "hospedagem", "nome", "email", "telefone", "assunto", "mensagem", "lida", "enviada_em"]
