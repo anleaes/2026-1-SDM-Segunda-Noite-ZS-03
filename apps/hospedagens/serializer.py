dacb3aa86930a65d61d8005452fd9c49751a635f
diegol447 <diegoamoreira33@gmail.com>
2026-05-31 14:16:18 -0300

    melhora da legibilidade

--- apps/hospedagens/serializer.py
+++ apps/hospedagens/serializer.py
@@ -6,4 +6,16 @@ from .models import Hospedagem
 class HospedagemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hospedagem
-        fields = ("id", "titulo", "descricao", "tipo", "endereco", "comodidades", "preco_diaria", "capacidade", "quartos", "banheiros", "ativo")
+        fields = (
+            "id",
+            "titulo",
+            "descricao",
+            "tipo",
+            "endereco",
+            "comodidades",
+            "preco_diaria",
+            "capacidade",
+            "quartos",
+            "banheiros",
+            "ativo",
+        )
