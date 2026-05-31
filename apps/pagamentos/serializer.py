cf9186fc2dba200000526b33af265235225bef7b
Vitor Appel Teixeira <vitorappel2005@gmail.com>
2026-05-31 12:26:54 -0300

    refactor(pagamentos): adicionei a validação para impedir pagamentos com valores negativos

--- apps/pagamentos/serializer.py
+++ apps/pagamentos/serializer.py
@@ -5,3 +5,8 @@ class PagamentoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pagamento
         fields = ["id", "reserva", "valor", "metodo", "status", "data_pagamento", "criado_em",] 
+
+    def validate_valor(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("O valor do pagamento deve ser maior que zero.")
+        return value
